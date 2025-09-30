8c4ca12b56ff48b8ec937d081f6b9e1bc8448415
DADDYEAGER1 <143586154+DADDYEAGER1@users.noreply.github.com>
2025-09-30 10:16:11 +0530

    Create next-sitemap.config.js

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://mirelleinspo.com',
+  generateRobotsTxt: true, // also generates robots.txt
+  sitemapSize: 5000,
+}

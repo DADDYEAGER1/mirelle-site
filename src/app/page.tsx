afd98be546a54f8ba6974eab225196b44fe6f24b
DADDYEAGER1 <143586154+DADDYEAGER1@users.noreply.github.com>
2025-10-10 04:17:26 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AboutSection from '@/components/AboutSection';
 import BlogSection from '@/components/BlogSection';
 import FeaturedTopicsSection from '@/components/FeaturedTopicsSection';
 import ShopSection from '@/components/ShopSection';
-import PinterestShowcaseSection from '@/components/PinterestShowcaseSection';
+// import PinterestShowcaseSection from '@/components/PinterestShowcaseSection';
 import SubscribeSection from '@/components/SubscribeSection';
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function Home() {
       <BlogSection />
       <FeaturedTopicsSection />
       <ShopSection />
-      <PinterestShowcaseSection />
+      {/* <PinterestShowcaseSection /> */}
       <SubscribeSection />
     </div>
   );

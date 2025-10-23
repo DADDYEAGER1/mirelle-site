7c525b8edacfde7a6f9eaf0da3b928debd879d36
DADDYEAGER1 <143586154+DADDYEAGER1@users.noreply.github.com>
2025-10-24 04:13:23 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -245,6 +245,9 @@ export default function RootLayout({
 
         {/* AdSense verification meta tag */}
         <meta name="google-adsense-account" content="ca-pub-1145734682794444" />
+        
+        {/* Pinterest Save Button Script */}
+        <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
       </head>
 
       <body className="antialiased">

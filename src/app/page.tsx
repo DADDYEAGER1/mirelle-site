5c12b3c65729552e2def4cc5b8110f0b7e9f369f
DADDYEAGER1 <143586154+DADDYEAGER1@users.noreply.github.com>
2025-11-28 01:26:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
       <AboutSection />
       <ShopSection />
       <FeaturedTopicsSection />
-      {/* <InspoShowcase />  */}
+      <InspoShowcase />
       <BlogSection />
       <SubscribeSection />
       

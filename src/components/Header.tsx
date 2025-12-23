0ee9efcce588df7feb9e2b0c73e6677cbbee9f3a
DADDYEAGER1 <143586154+DADDYEAGER1@users.noreply.github.com>
2025-12-24 01:32:39 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -70,11 +70,6 @@ export default function Header() {
                 >
                   WORK WITH US
                 </Link>
-                <button className="text-[#f9fafb] hover:opacity-70 transition-opacity">
-                  <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                </button>
               </div>
             </div>
           </div>
